48c44352fe13d97a3470ea46c2016ce6b719f589
unknown <candanoza170@gmail.com>
2019-08-21 11:14:51 -0500

    CobrosM

--- resources/views/matricular/total-alumnosCobros.blade.php
+++ resources/views/matricular/total-alumnosCobros.blade.php
@@ -13,28 +13,29 @@
 
 
 					{!! Form::open(['route' => 'matricular.total-alumnosStore']) !!}
-					<div class="panel panel-primary">
+					<div class="panel panel-primary" id="panel">
 						<div class="panel panel-heading text-center"></div>
 						<div class="panel panel-body">
 							<div class="form-row">
+                                <div>
 								<div class="form-group col-md-4">
 		<strong>Curso: <br></strong>
-		<div class="input-group-prepend">
+		<div class="input-group-prepend" id="curso">
 		<span class="input-group-text"><i class="fas fa-calendar-day"></i></span>
 		{!! Form::select('curso',['INICIAL 1' => 'INICIAL 1', 'INICIAL 2' => 'INICIAL 2', 'PRIMERO DE EGB' => 'PRIMERO DE EGB', 'SEGUNDO DE EGB' => 'SEGUNDO DE EGB', 'TERCERO DE EGB' => 'TERCERO DE EGB', 'CUARTO DE EGB' => 'CUARTO DE EGB', 'QUINTO DE EGB' => 'QUINTO DE EGB', 'SEXTO DE EGB' => 'SEXTO DE EGB', 'SEPTIMO DE EGB' => 'SEPTIMO DE EGB', 'OCTAVO DE EGB' => 'OCTAVO DE EGB', 'NOVENO DE EGB' => 'NOVENO DE EGB', 'DECIMO DE EGB' => 'DECIMO DE EGB', 'PRIMERO DE BACHILLERATO' => 'PRIMERO DE BACHILLERATO', 'SEGUNDO DE BACHILLERATO' => 'SEGUNDO DE BACHILLERATO', 'TERCERO DE BACHILLERATO' => 'TERCERO DE BACHILLERATO'], null, ['class' => 'form-control col-md-8', 'id' => 'curso', 'placeholder' => 'Ingrese el curso...']) !!}<br>
 		</div>
 		</div>
-		<div class="form-group col-md-4">
+		<div class="form-group col-md-4" id="paralelo">
 		<strong>Paralelo: <br></strong>
         <div class="input-group-prepend">
 					<span class="input-group-text"><i class="fas fa-id-card-alt"></i></span>
 		{!! Form::select('paralelo',['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E', 'F' => 'F', 'G' => 'G', 'H' => 'H', 'I' => 'I', 'J' => 'J'], null, ['class' => 'form-control col-md-8', 'id' => 'paralelo', 'placeholder' => 'Ingrese el paralelo']) !!}
 		</div>
         </div>
-        <div class="form-group col-md-12">
+        <div class="form-group col-md-12" id="busqueda">
                 {!! Form::button(' <i class="fas fa-search"></i> BUSQUEDA', ['class' => 'btn btn-primary', 'type' => 'submit', 'id' => 'mostrarBusqueda']) !!}
          </div>
-         <div class="form-group col-md-12">
+         <div class="form-group col-md-12" id="imprimir">
                 {!!Form::button('<i class="fas fa-print"></i> IMPRIMIR', ['class' => 'btn btn-primary', 'type' => 'button', 'id' => 'boton'])!!}
          </div>
 
@@ -60,7 +61,7 @@
 
 						</div>
 					</div>
-                   <table class="table table-hover table-striped" id="tableid">
+                   <table class="table table-hover table-striped" id="tableid" border="1">
                         <thead>
                                 <tr>
                                     <th>N.</th>
@@ -137,13 +138,18 @@
       } );
        </script>
        <script>
-            $('#boton').on('click', function(){
-                if($('#boton') != ''){
+            function printData()
+            {
+               var divToPrint=document.getElementById("tableid");
+               newWin= window.open("");
+               newWin.document.write(divToPrint.outerHTML);
+               newWin.print();
+               newWin.close();
+            }
 
-                         window.print();
-                }
-
-         });
+            $('#boton').on('click',function(){
+            printData();
+            })
 
         </script>
 @endsection
