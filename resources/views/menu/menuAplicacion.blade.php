e2809cb78cd0c058338e08a2169ad0aad74018f2
unknown <candanoza170@gmail.com>
2019-09-12 16:36:04 -0500

    Nuevo commit por la mañana

--- resources/views/menu/menuAplicacion.blade.php
+++ resources/views/menu/menuAplicacion.blade.php
@@ -128,7 +128,9 @@
               @can('notas.index')
                 <a class="dropdown-item" href="{{ route('notas.index') }}">Ingresar notas</a>
                 @endcan
+                @can('notas.editar.notas')
                 <a class="dropdown-item" href="{{ route('notas.editar-notas')}}">Editar notas</a>
+                @endcan
                 @can('notas.cargadas')
                 <a class="dropdown-item" href="{{ route('notas.cargadas') }}">Ver notas profesores</a>
                 @endcan
