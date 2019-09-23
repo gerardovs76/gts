769b9be19a26c6c8ff098fe507a770d24c6c9bf9
unknown <candanoza170@gmail.com>
2019-09-23 13:48:24 -0500

    nuevo commit

--- resources/views/pdf/reporte-matriculados.blade.php
+++ resources/views/pdf/reporte-matriculados.blade.php
@@ -29,7 +29,7 @@ table#mitabla td {
 		<img src="images/lp.PNG" alt="" height="80" width="80" style="float: left;">
 		<img src="images/ib.png" alt="" height="80" width="180" style="float: right;">
 <header style="text-align: center;"><h2>REPORTE MATRICULADOS INSCRITOS A UN CURSO: <BR></h2></header>
-	<table >
+	<table id="mitabla">
 		<thead>
 			<tr>
 				<th>NOMBRES</th>
