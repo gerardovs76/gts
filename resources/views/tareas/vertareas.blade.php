620177ec791b6e47624e3eb8f7554528f589dfd6
unknown <candanoza170@gmail.com>
2019-10-19 12:09:27 -0500

    Nuevo commit por la mañana

--- resources/views/tareas/vertareas.blade.php
+++ resources/views/tareas/vertareas.blade.php
@@ -38,18 +38,27 @@
     </div>-->
     @if(Auth::user()->isRole('alumno'))
 	<script>
-		$(document).ready(function(){
+            $(document).ready(function(){
 
-			$.get('tareas_matriculados', function(response){
-                console.log(response);
-				$.each(response, function(index, obj){
-					var url = 'tareas/download/'+obj.archivo;
-					$('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td><td><a  href='+url+'><i aling="center" class="fas fa-file-alt fa-2x"></i></a></td></tr>');
+                $.get('tareas_matriculados', function(response){
+                    console.log(response);
+                    $.each(response, function(index, obj){
+                        var url = 'tareas/download/'+obj.archivo;
+                        if(obj.archivo == 'no-exite')
+                        {
+                            $('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td></tr>');
 
-				});
+                        }
+                        else{
+                            $('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td><td><a  href='+url+'><i aling="center" class="fas fa-file-alt fa-2x"></i></a></td></tr>');
+                        }
 
-			});
-		});
+
+                    });
+
+                });
+            });
+
     </script>
     @elseif(Auth::user()->isRole('profesor'))
     <script>
@@ -76,30 +85,6 @@
 			});
 		});
     </script>
-    @elseif(Auth::user()->isRole('alumno'))
-    <script>
-            $(document).ready(function(){
-
-                $.get('tareas_matriculados', function(response){
-                    console.log(response);
-                    $.each(response, function(index, obj){
-                        var url = 'tareas/download/'+obj.archivo;
-                        if(obj.archivo == 'no-exite')
-                        {
-                            $('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td></tr>');
-
-                        }
-                        else{
-                            $('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td><td><a  href='+url+'><i aling="center" class="fas fa-file-alt fa-2x"></i></a></td></tr>');
-                        }
-
-
-                    });
-
-                });
-            });
-
-    </script>
     @elseif(Auth::user()->isRole('super-admin'))
     <script>
 		$(document).ready(function(){
@@ -108,6 +93,7 @@
 				$.each(response, function(index, obj){
                     var url = 'tareas/download/'+obj.archivo;
                     if(obj.archivo == 'no-exite')
+                    console.log(obj.archivo);
                     {
                         $('#tbody').append('<tr><td>'+obj.nombre_profesor+'</td><td>'+obj.curso+'</td><td>'+obj.paralelo+'</td><td>'+obj.fecha_entrega+'</td><td>'+obj.tipo_tarea+'</td><td>'+obj.titulo+'</td><td>'+obj.descripcion+'</td><td><a href="tareas/'+obj.id+'" class="btn btn-danger btn-sm">ELIMINAR</a></td></tr>');
 
