40778b71ddab7c65d67d0d3fa3345a7839f4f8e5
Willian Candanoza <candanoza170@gmail.com>
2019-10-28 11:36:02 -0500

    nuevo commit por la mañana

--- resources/views/inspecciones/alertas.blade.php
+++ resources/views/inspecciones/alertas.blade.php
@@ -43,7 +43,6 @@
                     <td></td>
                     <td>{{($in->h1_count_03 +$in->h2_count_03 +$in->h3_count_03 +$in->h4_count_03 +$in->h5_count_03 +$in->h6_count_03 +$in->h7_count_03 +$in->h8_count_03 )}}</td>
                     <td>{{($in->h1_count_04 +$in->h2_count_04 +$in->h3_count_04 +$in->h4_count_04 +$in->h5_count_04 +$in->h6_count_04 +$in->h7_count_04 +$in->h8_count_04 )}}</td>
-                    <td><a href="matricular-perfil-total" class="btn btn-primary"><i class="fa fa-paper-plane"></i> IR AL PERFIL</a></td>
                     @endif
                 </tr>
                 @endforeach
