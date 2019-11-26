528169a38cef0cbd687a03be72d6129bad4d3b3e
Willian Candanoza <candanoza170@gmail.com>
2019-11-26 11:26:08 -0500

    asdasd

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -141,7 +141,7 @@ class NotasController extends Controller
         $matriculados_id = $request->matriculados_id;
         $materias_id = $request->materias_id;
 
-        foreach($matriculados_id as $key => $value)
+        /* foreach($matriculados_id as $key => $value)
         {
             $notas_antiguas = Nota_ta::where('materias_id', $materias_id)->where('matriculado_id', $matriculados_id[$key])->where('parcial', $parcial)->where('quimestre', $quimestre)->delete();
             $nota_ta = new Nota_ta;
@@ -167,10 +167,9 @@ class NotasController extends Controller
             $nota_ta->numero_tarea_ta5 = '1';
             $nota_ta->save();
         }
-        $i = 0;
+        $i = 0; */
         foreach($matriculados_id as $key => $value)
         {
-            dd($i++);
             $notas_antiguas = Notas_ti::where('materias_id', $materias_id)->where('matriculado_id', $matriculados_id[$key])->where('parcial', $parcial)->where('quimestre', $quimestre)->delete();
             $nota_ti = new Notas_ti;
             $nota_ti->nota_ti1 = ($nota_ti1[$key] == '' ? 0 : $nota_ti1[$key]);
