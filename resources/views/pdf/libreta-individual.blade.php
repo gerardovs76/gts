b81b111b12ae01535a90f7d98d597faad569ff5d
Willian Candanoza <candanoza170@gmail.com>
2019-11-27 02:08:10 -0500

    asdasd

--- resources/views/pdf/libreta-individual.blade.php
+++ resources/views/pdf/libreta-individual.blade.php
@@ -265,10 +265,18 @@ table#mitabla3 td {
 			<td>{{$notas_ev->nota_final_ev}}</td>
 			@endif
 			@endforeach
-			@foreach($nota->notas_ta as $c => $value)
-			@if($materia->id == $nota->notas_ta[$c]->materias_id && $materia->id == $nota->notas_ti[$c]->materias_id && $materia->id == $nota->notas_tg[$c]->materias_id && $materia->id == $nota->notas_le[$c]->materias_id && $materia->id == $nota->notas_ev[$c]->materias_id)
-			<td>{{round(((($nota->notas_ta[$c]->nota_final_ta)  +  ($nota->notas_ti[$c]->nota_final_ti)  +  ($nota->notas_tg[$c]->nota_final_tg)  +  ($nota->notas_le[$c]->nota_final_le)  + ($nota->notas_ev[$c]->nota_final_ev)) / 5),3)}}</td>
+			@foreach($nota->notas_ta as $notas_ta)
+			@foreach($nota->notas_ti as $notas_ti)
+			@foreach($nota->notas_tg as $notas_tg)
+			@foreach($nota->notas_le as $notas_le)
+			@foreach($nota->notas_ev as $notas_ev)
+			@if($materia->id == $notas_ta->materias_id && $materia->id == $notas_ti->materias_id && $materia->id == $notas_tg->materias_id && $materia->id == $notas_le->materias_id && $materia->id == $notas_ev->materias_id)
+			<td>{{round(((($notas_ta->nota_final_ta)  +  ($notas_ti->nota_final_ti)  +  ($notas_tg->nota_final_tg)  +  ($notas_le->nota_final_le)  +  ($notas_ev->nota_final_ev)) / 5),3)}}</td>
 			@endif
+			@endforeach
+			@endforeach
+			@endforeach
+			@endforeach
 			@endforeach 
 			</tr>
 			@endif
