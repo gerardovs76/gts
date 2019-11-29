5043a807da42aa03f5251e06cc6efe85e8bcf0d8
Willian Candanoza <candanoza170@gmail.com>
2019-11-29 10:54:45 -0500

    asdasd

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -1416,6 +1416,7 @@ class NotasController extends Controller
         }])->with(['inscripcion' => function($query8){
             $query8->select('cedula', 'nombres_representante'); 
         }])->where('curso', $curso)->where('paralelo', $paralelo)->groupBy('id')->orderBy('apellidos')->get();
+        dd($notas);
         $notasPromedioFinalTa = [];
         $notasPromedioFinalTi = [];
         $notasPromedioFinalTg = [];
