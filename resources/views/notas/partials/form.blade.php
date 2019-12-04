b2c037aa907ca0b6537755eefc2ab3b4773b86ca
Willian Candanoza <candanoza170@gmail.com>
2019-12-04 16:13:40 -0500

    asdasd

--- resources/views/notas/partials/form.blade.php
+++ resources/views/notas/partials/form.blade.php
@@ -181,10 +181,11 @@
                                                        var sumaTa = ((nota_ta1 + nota_ta2 + nota_ta3 + nota_ta4 + nota_ta5));
                                                        var sumaTa2 = ((nota_ta1 == 0 ? 0 : 1) +(nota_ta2 == 0 ? 0 : 1) +(nota_ta3 == 0 ? 0 : 1) +(nota_ta4 == 0 ? 0 : 1) +(nota_ta5 == 0 ? 0 : 1));
                                                        var sumaTaTotal = (sumaTa / sumaTa2);
+                                                       sumaTaTotal.toFixed(2);
                                                        $('#promedioTa').on('click', () => {
                                                             $('#promediota'+obj.id+'').empty();
                                                             $('#promediota'+obj.id+'').css("font-size", "12px");
-                                                             $('#promediota'+obj.id+'').val(sumaTaTotal.toFixed(2));
+                                                             $('#promediota'+obj.id+'').val(sumaTaTotal);
                                                        });
                                                   }, 500);
                                                   setInterval(() => {
