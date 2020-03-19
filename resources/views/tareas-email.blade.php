5aaf1aa2def103b2b083c941e95e621cb60e6967
Willian Candanoza <candanoza170@gmail.com>
2020-03-19 09:36:07 -0500

    nuevo commit

--- resources/views/tareas-email.blade.php
+++ resources/views/tareas-email.blade.php
@@ -1,5 +1,5 @@
 <h1>GTS INFORMA.-</h1>
-<h3>{{$body}}</h3>
+<h3>{{$mensaje}}</h3>
 <h3>Por favor ingresar a la plataforma para la revisión de novedades.</h3>
 <h4>Para soporte visite www.gtsenlinea.com o llame al 022684848</h4>
 <h4>Este es un servicio de GTS.</h4>
